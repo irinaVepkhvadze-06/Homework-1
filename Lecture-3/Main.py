33fc1e0dc78e2c8ad99bebdcffbb41502bb67484
irinaVepkhvadze-06 <irinavepkhvadze@gmail.com>
2026-09-19 16:46:23 +0400

    Update homework

---
+++ Lecture-3/Main.py
@@ -0,0 +1,58 @@
+##########################
+#String Index
+##########################
+
+# word = "Python"
+# print(word[0])
+# print(word[1])
+# print(word[-1])
+# #print(word[10]) #index error
+# print(word[-2])
+
+
+##########################
+#String Slicing
+##########################
+
+
+# text = "Python Programming"
+# print(text[0:5])
+# print(text[0:6])
+# print(text[0:8])
+# print(text[-11:-1])
+# print(text[-11:])
+# print(text[7:])
+# print(text[:7])
+# print(text[0:13:2])
+# print(text[::2])
+# print(text[::-1])
+
+
+# word1 = "Python" 
+# word1 += "Programming"
+# print(word1)
+# print(id(word1))
+
+
+##########################
+#String Method
+##########################
+
+txt = "  python is programming language  " 
+# txt = txt.upper()
+# # print(txt)
+# print(txt.lower())
+# print(txt.capitalize())
+# print(txt.title())
+# print(txt.find("python"))
+# print(txt.replace("python", "Java"))
+# print(txt.split())
+
+# print("Hello", "World")
+
+# name: Str = input("Enter your name: ")
+# print(f"Hello, {name}")
+
+##########################
+#F-Strings
+##########################
--- Lecture/Main.py
+++ Lecture/Main.py
@@ -1,63 +1,64 @@
 
 
 
-print(10)
-print(10.5)
-print(type(10))
-print(type(10.5))
+# print(10)
+# print(10.5)
+# print(type(10))
+# print(type(10.5))
 
 
-print(10 + 5)
-print( 10 - 5)
-print ( 10*5)
-print(10 + 5.4 )
-print(10.5 - 5.4)
-print(10 * 5.4)
-print(10 / 2)
-print(10 // 3)
-print(10 % 3)
-print( 10 ** 3)
-print(0.1 + 0.3)
-print(0.1 + 0.2)
-print(5 % 2)
-print(5 % -2)
-print(-5 % 2)
-print(5 // 2) #2.5
-print(5 // -2) # -2.5
+# print(10 + 5)
+# print( 10 - 5)
+# print ( 10*5)
+# print(10 + 5.4 )
+# print(10.5 - 5.4)
+# print(10 * 5.4)
+# print(10 / 2)
+# print(10 // 3)
+# print(10 % 3)
+# print( 10 ** 3)
+# print(0.1 + 0.3)
+# print(0.1 + 0.2)
+# print(5 % 2)
+# print(5 % -2)
+# print(-5 % 2)
+# print(5 // 2) #2.5
+# print(5 // -2) # -2.5
 
 
 
-#print(True)
-#print(False)
+# #print(True)
+# #print(False)
 
-print(10 > 5)
-print(10 < 5)
-print(10 == 5)
-print(10 !=5)
-print(10 >=5)
-print( 10 <=5)
+# print(10 > 5)
+# print(10 < 5)
+# print(10 == 5)
+# print(10 !=5)
+# print(10 >=5)
+# print( 10 <=5)
 
-#####################
-# NoneType / None
-####################
+# #####################
+# # NoneType / None
+# ####################
 
-print(None)
-print(type(None))
+# print(None)
+# print(type(None))
 
 
 ####################
 # Variables
 ####################
 
-num1=5
-num2=2
-print(num1)
-print(num2)
-count = 3
-print(count)
+# num1=5
+# num2=2
+# print(num1)
+# print(num2)
+# count = 3
+# print(count)
 
 
-Age=32
-print(Age)
+# Age=32
+# print(Age)
 
 
+print(int('123'))
